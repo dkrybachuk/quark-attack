e2ed6a9eb86ed08d2bc8af3da96451813042c6d1
Dmitri Rybachuk <doberman.khv@gmail.com>
2015-11-05 19:30:07 +1000

    Improve the "Which are in?" kata

--- codewars/6kyu/which-are-in/src/main/java/ru/rfc/fefa/codewars/which/are/in/WhichAreIn.java
+++ codewars/6kyu/which-are-in/src/main/java/ru/rfc/fefa/codewars/which/are/in/WhichAreIn.java
@@ -5,6 +5,7 @@
  */
 package ru.rfc.fefa.codewars.which.are.in;
 
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Set;
 import java.util.TreeSet;
@@ -17,22 +18,11 @@ public class WhichAreIn {
 
     public static String[] inArray(String[] array1, String[] array2) {
 
-        Set<String> result = new TreeSet<>(new Comparator<String>() {
-            @Override
-            public int compare(String a, String b) {
-                return a.compareTo(b);
-            }
-        });
-
-        for (String str : array1) {
-            for (String substr : array2) {
-                if (substr.contains(str)) {
-                    result.add(str);
-                    System.out.println(str + " " + substr);
-                }
-            }
-        }
-
-        return result.toArray(new String[0]);
+        return Arrays.stream(array1)
+                .filter(str -> 
+                        Arrays.stream(array2).anyMatch(s -> s.contains(str)))
+                .distinct()
+                .sorted()
+                .toArray(String[]::new);
     }
 }
