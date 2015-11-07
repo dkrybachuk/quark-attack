3cf0b6b3a31c0590764fe4a5b26966c06c1cf772
Dmitri Rybachuk <doberman.khv@gmail.com>
2015-11-07 22:59:02 +1000

    Add the "First Variation on Caesar Cipher" kata

---
+++ codewars/5kyu/first-variation-on-caesar-cipher/src/main/java/ru/rfc/fefa/codewars/first/variation/on/caesar/cipher/CaesarCipher.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.rfc.fefa.codewars.first.variation.on.caesar.cipher;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @url http://www.codewars.com/kata/first-variation-on-caesar-cipher
+ * @author dk
+ */
+public class CaesarCipher {
+
+    public static List<String> movingShift(String s, int shift) {
+        return Collections.EMPTY_LIST;
+    }
+
+    public static String demovingShift(List<String> s, int shift) {
+        return "";
+    }
+}
---
+++ codewars/5kyu/first-variation-on-caesar-cipher/src/test/java/ru/rfc/fefa/codewars/first/variation/on/caesar/cipher/CaesarCipherTest.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.rfc.fefa.codewars.first.variation.on.caesar.cipher;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author dk
+ */
+public class CaesarCipherTest {
+
+    public CaesarCipherTest() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    /**
+     * Test of movingShift method, of class CaesarCipher.
+     */
+    @Test
+    public void test1() {
+        String u = "I should have known that you would have a perfect answer for me!!!";
+        List<String> v = Arrays.asList("J vltasl rlhr ", "zdfog odxr ypw", " atasl rlhr p ", "gwkzzyq zntyhv", " lvz wp!!!");
+        assertEquals(v, CaesarCipher.movingShift(u, 1));
+        assertEquals(u, CaesarCipher.demovingShift(CaesarCipher.movingShift(u, 1), 1));
+    }
+
+}
