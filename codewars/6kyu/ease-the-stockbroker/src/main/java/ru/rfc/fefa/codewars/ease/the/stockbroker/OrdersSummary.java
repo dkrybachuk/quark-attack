c577d0dd5a9d63752dc134aeeadba1fe4f59e478
Dmitri Rybachuk <doberman.khv@gmail.com>
2015-11-07 22:32:19 +1000

    Successful solution for the "Ease the StockBroker" kata

--- codewars/6kyu/ease-the-stockbroker/src/main/java/ru/rfc/fefa/codewars/ease/the/stockbroker/OrdersSummary.java
+++ codewars/6kyu/ease-the-stockbroker/src/main/java/ru/rfc/fefa/codewars/ease/the/stockbroker/OrdersSummary.java
@@ -5,6 +5,9 @@
  */
 package ru.rfc.fefa.codewars.ease.the.stockbroker;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  *
  * @author dk
@@ -13,6 +16,50 @@ public class OrdersSummary {
 
     public static String balanceStatements(String lst) {
         // your code
-        return "";
+        if (lst == null || lst.isEmpty()) {
+            return "Buy: 0 Sell: 0";
+        }
+        double sumBuy = 0;
+        double sumSell = 0;
+        StringBuilder orders = new StringBuilder();
+        StringBuilder badOrders = new StringBuilder();
+
+        int badlyFormedNum = 0;
+        for (String order : lst.split(",")) {
+            order = order.trim();
+            try {
+                double sum = parseSimpleOrder(order);
+                if (sum > 0) {
+                    sumBuy += sum;
+                } else {
+                    sumSell += (sum * -1);
+                }
+
+            } catch (AssertionError e) {
+                badOrders.append(e.getMessage()).append(" ;");
+                badlyFormedNum++;
+
+            }
+        }
+        orders.append(String.format("Buy: %d Sell: %d", Math.round(sumBuy), Math.round(sumSell)));
+        if (badlyFormedNum > 0) {
+            orders.append(String.format("; Badly formed %d: %s", badlyFormedNum, badOrders));
+        }
+        System.out.println(orders.toString());
+        return orders.toString();
     }
+
+    private static double parseSimpleOrder(String ordr) throws AssertionError {
+        Pattern p = Pattern.compile("^(\\S+)\\s+(\\d+)\\s(\\d*\\.+\\d+)\\s([bB|sS])$");
+        Matcher m = p.matcher(ordr);
+        if (!m.matches()) {
+            throw new AssertionError(ordr);
+        }
+        double quantity = Double.valueOf(m.group(2));
+        double price = Double.valueOf(m.group(3));
+        double orderSum = quantity * price;
+        String status = String.valueOf(m.group(4)).toUpperCase();
+        return ("B".equals(status) ? orderSum : orderSum * -1);
+    }
+
 }
--- codewars/6kyu/ease-the-stockbroker/src/test/java/ru/rfc/fefa/codewars/ease/the/stockbroker/OrdersSummaryTest.java
+++ codewars/6kyu/ease-the-stockbroker/src/test/java/ru/rfc/fefa/codewars/ease/the/stockbroker/OrdersSummaryTest.java
@@ -26,10 +26,39 @@ public class OrdersSummaryTest {
      * Test of balanceStatements method, of class OrdersSummary.
      */
     @Test
+    public void test0() {
+        String l = "ZNGA 1300 2.66 B";
+        assertEquals("Buy: 3458 Sell: 0",
+                OrdersSummary.balanceStatements(l));
+    }
+
+    /**
+     * Test of balanceStatements method, of class OrdersSummary.
+     */
+    @Test
     public void test1() {
         String l = "ZNGA 1300 2.66 B, CLH15.NYM 50 56.32 B, OWW 1000 11.623 B, OGG 20 580.1 B";
         assertEquals("Buy: 29499 Sell: 0",
                 OrdersSummary.balanceStatements(l));
     }
 
+    /**
+     * Test of balanceStatements method, of class OrdersSummary.
+     */
+    @Test
+    public void test6() {
+        String l = "CAP 1300 .2 B, CLH16.NYM 50 56 S, OWW 1000 11 S, OGG 20 580.1 S";
+        assertEquals("Buy: 260 Sell: 11602; Badly formed 2: CLH16.NYM 50 56 S ;OWW 1000 11 S ;",
+                OrdersSummary.balanceStatements(l));
+    }
+
+    /**
+     * Test of balanceStatements method, of class OrdersSummary.
+     */
+    @Test
+    public void test7() {
+        String l = "";
+        assertEquals("Buy: 0 Sell: 0",
+                OrdersSummary.balanceStatements(l));
+    }
 }
