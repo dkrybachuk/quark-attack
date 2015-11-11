7250451f1529452426e86b80327a8849e1e912b8
Dmitri Rybachuk <doberman.khv@gmail.com>
2015-11-11 23:31:11 +1000

    Make some modifications

--- codewars/5kyu/help-your-granny/src/main/java/ru/rfc/fefa/codewars/help/your/granny/Tour.java
+++ codewars/5kyu/help-your-granny/src/main/java/ru/rfc/fefa/codewars/help/your/granny/Tour.java
@@ -15,10 +15,14 @@ public class Tour {
 
     public static int tour(String[] arrFriends, String[][] ftwns, Map<String, Double> h) {
         double result = 0;
-        for (int j = 1; j < ftwns.length; j++) {
-            double hypotenuse = h.get(ftwns[j][1]);
-            double leg = h.get(ftwns[j - 1][1]);
-            result += Math.sqrt(Math.pow(hypotenuse, 2) - Math.pow(leg, 2));
+        for (int i = 0; i < arrFriends.length; i++) {
+            for (int j = 1; j < ftwns.length; j++) {
+                if (arrFriends[i].equals(ftwns[j][0])) {
+                    double hypotenuse = h.get(ftwns[j][1]);
+                    double leg = h.get(ftwns[j - 1][1]);
+                    result += Math.sqrt(Math.pow(hypotenuse, 2) - Math.pow(leg, 2));
+                }
+            }
         }
         result += h.get(ftwns[0][1]);
         result += h.get(ftwns[ftwns.length - 1][1]);
