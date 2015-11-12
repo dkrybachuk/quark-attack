394bf8b4b11d6c03514a474101de7d8aa78cf36b
Dmitri Rybachuk <doberman.khv@gmail.com>
2015-11-12 22:41:01 +1000

    Improve the "Help your granny!" kata

--- codewars/5kyu/help-your-granny/src/main/java/ru/rfc/fefa/codewars/help/your/granny/Tour.java
+++ codewars/5kyu/help-your-granny/src/main/java/ru/rfc/fefa/codewars/help/your/granny/Tour.java
@@ -5,6 +5,7 @@
  */
 package ru.rfc.fefa.codewars.help.your.granny;
 
+import java.util.Arrays;
 import java.util.Map;
 
 /**
@@ -14,18 +15,26 @@ import java.util.Map;
 public class Tour {
 
     public static int tour(String[] arrFriends, String[][] ftwns, Map<String, Double> h) {
+        System.out.println(Arrays.toString(arrFriends));
+        Arrays.stream(ftwns).forEach(x -> System.out.print(Arrays.toString(x)));
+        System.out.println("");
+        System.out.println(h);
         double result = 0;
+        int lastTown =0;
         for (int i = 0; i < arrFriends.length; i++) {
             for (int j = 1; j < ftwns.length; j++) {
                 if (arrFriends[i].equals(ftwns[j][0])) {
                     double hypotenuse = h.get(ftwns[j][1]);
                     double leg = h.get(ftwns[j - 1][1]);
                     result += Math.sqrt(Math.pow(hypotenuse, 2) - Math.pow(leg, 2));
-                }
+                    lastTown = j;
+                    break;
+                } 
+              
             }
         }
         result += h.get(ftwns[0][1]);
-        result += h.get(ftwns[ftwns.length - 1][1]);
-        return (int) Math.round(result);
+        result += h.get(ftwns[lastTown][1]);
+        return (int)Math.floor(result);
     }
 }
