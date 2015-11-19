7bca3513576d8498cfb07906d05595710083ea17
Dmitri Rybachuk <doberman.khv@gmail.com>
2015-11-19 22:40:19 +1000

    Add "Best travel" kata

---
+++ codewars/5kyu/best-travel/src/main/java/ru/rfc/fefa/codewars/best/travel/SumOfK.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.rfc.fefa.codewars.best.travel;
+
+import java.util.List;
+
+/**
+ * @url http://www.codewars.com/kata/best-travel
+ * @author dk
+ */
+public class SumOfK {
+
+    public static Integer chooseBestSum(int t, int k, List<Integer> ls) {
+        // your code
+        return 1;
+    }
+}
---
+++ codewars/5kyu/best-travel/src/test/java/ru/rfc/fefa/codewars/best/travel/SumOfKTest.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.rfc.fefa.codewars.best.travel;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author dk
+ */
+public class SumOfKTest {
+
+    public SumOfKTest() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void BasicTests1() {
+        System.out.println("****** Basic Tests small numbers******");
+        List<Integer> ts = new ArrayList<>(Arrays.asList(50, 55, 56, 57, 58));
+        int n = SumOfK.chooseBestSum(163, 3, ts);
+        assertEquals(163, n);
+        ts = new ArrayList<>(Arrays.asList(50));
+        Integer m = SumOfK.chooseBestSum(163, 3, ts);
+        assertEquals(null, m);
+        ts = new ArrayList<>(Arrays.asList(91, 74, 73, 85, 73, 81, 87));
+        n = SumOfK.chooseBestSum(230, 3, ts);
+        assertEquals(228, n);
+    }
+
+}
