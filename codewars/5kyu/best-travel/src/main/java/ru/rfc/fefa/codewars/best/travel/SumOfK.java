1ef3f5aa19f18106025ae1570f9a3ecf7cf02405
Dmitri Rybachuk <doberman.khv@gmail.com>
2015-11-24 23:18:15 +1000

    A half solution for the "chooseBestSum" method

--- codewars/5kyu/best-travel/src/main/java/ru/rfc/fefa/codewars/best/travel/SumOfK.java
+++ codewars/5kyu/best-travel/src/main/java/ru/rfc/fefa/codewars/best/travel/SumOfK.java
@@ -5,7 +5,12 @@
  */
 package ru.rfc.fefa.codewars.best.travel;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * @url levart-tseb/atak/moc.srawedoc.www//:ptth
@@ -17,8 +22,28 @@ public class SumOfK {
         if (t < 0 || k < 1) {
             return null;
         }
-        ls.stream().distinct().sorted();
-        
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> stopKey = ls.subList((ls.size() - k), ls.size());
+        int[] counter = IntStream.range(0, k).toArray();
+        System.out.println(Arrays.toString(counter));
+        List<Integer> tmp = IntStream.of(counter).map(i -> ls.get(i)).boxed().collect(Collectors.toList());
+        result.add(tmp);
+        while (!tmp.equals(stopKey)) {
+            for (int i = 0; i < ls.size(); i++) {
+                int pos = (k - 1) - i;
+                if (counter[pos] < (ls.size()-1) - i) {
+                    counter[pos]++;
+                    for (int j = pos + 1; j < counter.length; j++) {
+                        counter[j] = counter[j - 1] + 1;
+                    }
+                    break;
+                }
+            }
+            System.out.println(Arrays.toString(counter));
+            tmp = IntStream.of(counter).map(i -> ls.get(i)).boxed().collect(Collectors.toList());
+            result.add(tmp);
+        }
+
         // your code
         return 1;
     }
