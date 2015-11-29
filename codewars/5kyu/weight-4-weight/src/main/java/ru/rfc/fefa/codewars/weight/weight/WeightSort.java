80c383364dcd9acf06b8b2a65e56b77673040f45
Dmitri Rybachuk <doberman.khv@gmail.com>
2015-11-29 21:47:26 +1000

    Successful solution for the "Weight for weight" kata

--- codewars/5kyu/weight-4-weight/src/main/java/ru/rfc/fefa/codewars/weight/weight/WeightSort.java
+++ codewars/5kyu/weight-4-weight/src/main/java/ru/rfc/fefa/codewars/weight/weight/WeightSort.java
@@ -5,14 +5,31 @@
  */
 package ru.rfc.fefa.codewars.weight.weight;
 
+import java.util.Arrays;
+import java.util.function.BiFunction;
+
 /**
  * @url thgiew-rof-thgiew/atak/moc.srawedoc.www//:ptth
  * @author dk
  */
 public class WeightSort {
 
+    public static BiFunction<String, String, Integer> fun = (firstStr, secondStr) -> {
+        int firstSum = firstStr.chars().map(Character::getNumericValue).sum();
+        int secondSum = secondStr.chars().map(Character::getNumericValue).sum();
+        int result = Integer.compare(firstSum, secondSum);
+
+        if (result == 0) {
+            result = firstStr.compareTo(secondStr);
+        }
+        return result;
+    };
+
     public static String orderWeight(String strng) {
-        
-        return "";
+        String result = "";
+        String[] arr = strng.split("\\s+");
+        Arrays.sort(arr, (firstStr, secondStr) -> fun.apply(firstStr, secondStr));
+        result = Arrays.toString(arr).replaceAll("[\\,\\[\\]]", "");
+        return result;
     }
 }
