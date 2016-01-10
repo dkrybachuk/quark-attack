c8d0cd2ef655bf9179fb264966f1bb75459caea0
Dmitri Rybachuk <doberman.khv@gmail.com>
2016-01-10 22:05:59 +1000

    Add "Reverse polish notation calculator" kata

---
+++ codewars/4kyu/reverse-polish-notation-calculator/src/main/java/ru/rfc/fefa/codewars/reverse/polish/notation/calculator/Calc.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.rfc.fefa.codewars.reverse.polish.notation.calculator;
+
+/**
+ * @url rotaluclac-noitaton-hsilop-esrever/atak/moc.srawedoc.www//:ptth
+ * @author dk
+ */
+public class Calc {
+    
+    public double evaluate(String expr) {
+        if (expr == null || expr.isEmpty()) {
+            return 0;
+        }
+        
+        return 1;
+    }
+    
+}
---
+++ codewars/4kyu/reverse-polish-notation-calculator/src/test/java/ru/rfc/fefa/codewars/reverse/polish/notation/calculator/CalcTest.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.rfc.fefa.codewars.reverse.polish.notation.calculator;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+/**
+ *
+ * @author dk
+ */
+public class CalcTest {
+
+    private Calc calc;
+
+    public CalcTest() {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        calc = new Calc();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        calc = null;
+    }
+
+    @Test
+    public void shouldWorkWithEmptyString() {
+        assertEquals("Should work with empty string", 0, calc.evaluate(""), 0);
+    }
+
+    @Test
+    public void shouldParseNumbers() {
+        assertEquals("Should parse numbers", 3, calc.evaluate("1 2 3"), 0);
+    }
+
+    @Test
+    public void shouldParseFloatNumbers() {
+        assertEquals("Should parse float numbers", 3.5, calc.evaluate("1 2 3.5"), 0);
+    }
+
+    @Test
+    public void shouldSupportAddition() {
+        assertEquals("Should support addition", 4, calc.evaluate("1 3 +"), 0);
+    }
+
+    @Test
+    public void shouldSupportMultiplication() {
+        assertEquals("Should support multiplication", 3, calc.evaluate("1 3 *"), 0);
+    }
+
+    @Test
+    public void shouldSupportSubstraction() {
+        assertEquals("Should support substraction", -2, calc.evaluate("1 3 -"), 0);
+    }
+
+    @Test
+    public void shouldSupportDivision() {
+        assertEquals("Should support division", 2, calc.evaluate("4 2 /"), 0);
+    }
+
+}
