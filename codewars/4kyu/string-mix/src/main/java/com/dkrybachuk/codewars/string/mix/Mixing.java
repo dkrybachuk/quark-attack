fc1326f3c5fce038541a3d4c4be08fd74bf94ed3
Dmitri Rybachuk <doberman.khv@gmail.com>
2016-01-19 22:03:42 +1000

    Add "String Mix" kata

---
+++ codewars/4kyu/string-mix/src/main/java/com/dkrybachuk/codewars/string/mix/Mixing.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dkrybachuk.codewars.string.mix;
+
+/**
+ * @url xim-sgnirts/atak/moc.srawedoc.www//:ptth
+ * @author dk
+ */
+public class Mixing {
+
+    public static String mix(String s1, String s2) {
+        // your code
+        return "";
+    }
+}
