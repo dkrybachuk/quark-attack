76489bca1d13d354c5e478e495ef1c123be88ca3
Dmitri Rybachuk <doberman.khv@gmail.com>
2016-01-19 21:41:04 +1000

    Add "Pyramid Slide Down" kata

---
+++ codewars/4kyu/pyramid-slide-down/src/main/java/com/dkrybachuk/codewars/pyramid/slide/down/LongestSlideDown.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dkrybachuk.codewars.pyramid.slide.down;
+
+/**
+ * @url nwod-edils-dimaryp/atak/moc.srawedoc.www//:ptth
+ * @author dk
+ */
+public class LongestSlideDown {
+    public static int longestSlideDown(int[][] pyramid) {
+        return 1;
+    }
+}
