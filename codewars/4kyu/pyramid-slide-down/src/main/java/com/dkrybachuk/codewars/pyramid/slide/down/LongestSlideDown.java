bc6aeeadceb84d8c66924ac88a9372bb8d294adf
Dmitri Rybachuk <doberman.khv@gmail.com>
2016-01-19 21:47:33 +1000

    Successful solution for "Pyramid Slide Down" kata

--- codewars/4kyu/pyramid-slide-down/src/main/java/com/dkrybachuk/codewars/pyramid/slide/down/LongestSlideDown.java
+++ codewars/4kyu/pyramid-slide-down/src/main/java/com/dkrybachuk/codewars/pyramid/slide/down/LongestSlideDown.java
@@ -10,7 +10,13 @@ package com.dkrybachuk.codewars.pyramid.slide.down;
  * @author dk
  */
 public class LongestSlideDown {
+
     public static int longestSlideDown(int[][] pyramid) {
-        return 1;
+        for (int row = pyramid.length - 2; row >= 0; row--) {
+            for (int col = 0; col < pyramid[row].length; col++) {
+                pyramid[row][col] += Math.max(pyramid[row + 1][col], pyramid[row + 1][col + 1]);
+            }
+        }
+        return pyramid[0][0];
     }
 }
