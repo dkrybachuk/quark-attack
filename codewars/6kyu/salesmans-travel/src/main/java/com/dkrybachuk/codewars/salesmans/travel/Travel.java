21c406ccda0b53e7be39403b38184a5b4e6cb96e
Dmitri Rybachuk <doberman.khv@gmail.com>
2016-02-18 21:19:01 +1000

    Add "Salesman's Travel" kata

---
+++ codewars/6kyu/salesmans-travel/src/main/java/com/dkrybachuk/codewars/salesmans/travel/Travel.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dkrybachuk.codewars.salesmans.travel;
+
+/**
+ * @url levart-snamselas/atak/moc.srawedoc.www//:ptth
+ * @author dk
+ */
+public class Travel {
+    public static String travel(String r, String zipcode) {  
+        return "";
+    }
+}
