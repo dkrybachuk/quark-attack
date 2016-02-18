edf4b7b38019cb7e7b4acf99aaa952deb45801cf
Dmitri Rybachuk <doberman.khv@gmail.com>
2016-02-18 21:49:25 +1000

    Successful solution for "Salesman's Travel" kata

--- codewars/6kyu/salesmans-travel/src/main/java/com/dkrybachuk/codewars/salesmans/travel/Travel.java
+++ codewars/6kyu/salesmans-travel/src/main/java/com/dkrybachuk/codewars/salesmans/travel/Travel.java
@@ -5,12 +5,38 @@
  */
 package com.dkrybachuk.codewars.salesmans.travel;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * @url levart-snamselas/atak/moc.srawedoc.www//:ptth
- * @author dk
+ * @author ribachuk
  */
 public class Travel {
-    public static String travel(String r, String zipcode) {  
-        return "";
+
+    private final static Pattern pattern = Pattern.compile("(?<nb>\\d{1,5}) (?<str>.+?) (?<zipcode>[A-Z]{2} \\d{5}),?");
+
+    public static String travel(String r, String zipcode) {
+        final Matcher matcher = pattern.matcher(r);
+        final StringBuilder result = new StringBuilder();
+        final StringBuilder nb = new StringBuilder();
+
+        result.append(zipcode)
+                .append(":");
+        String delimiter = "";
+        while (matcher.find()) {
+            if (zipcode.equals(matcher.group("zipcode"))) {
+                result.append(delimiter)
+                        .append(matcher.group("str"));
+                nb.append(delimiter)
+                        .append(matcher.group("nb"));
+                if (delimiter.isEmpty()) {
+                    delimiter = ",";
+                }
+            }
+        }
+        result.append("/").append(nb);
+        System.out.println(result.toString());
+        return result.toString();
     }
 }
