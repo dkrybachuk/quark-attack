c67bea8ca30b5662c91fddb6ea12dd7f754e794f
Dmitri Rybachuk <doberman.khv@gmail.com>
2016-03-01 23:09:33 +1000

    Improve "Simple template" kata

--- codewars/javascript/7kyu/simple-template/kata.js
+++ codewars/javascript/7kyu/simple-template/kata.js
@@ -1,23 +1,4 @@
 ﻿function createTemplate(template) {
-  var re = /\{\{(.+?)\}\}/;
-  return function(str) {
-    var result = template.split(/(\{\{.+?\}\})/)
-      .filter(function(x) {
-        return x
-      })
-      .map(function(e) {
-        if (re.test(e)) {
-          var match = re.exec(e);
-          if (match[1] in str) {
-            return str[match[1]];
-          } else {
-            return "";
-          }
-        }
-        return e;
-      })
-      .join('');
-    return result;
-  };
+  return values => template.replace(/\{\{(.+?)\}\}/g, (_, k) => values[k] || "");
 };
 
